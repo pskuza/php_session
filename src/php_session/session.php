1ea4ecf693aed2776dd16b33b78915f9a2547cc2
Skuza <skuza@emonit.de>
2017-05-31 13:03:35 +0200

    first working version

--- src/php_session/session.php
+++ src/php_session/session.php
@@ -11,29 +11,28 @@ class session extends \SessionHandler
 
     protected $session_cache_identifier = "php_session_";
 
-    public function __construct(\ParagonIE\EasyDB\EasyDB $db, $session_cache)
+    protected $cachetime = 3600;
+
+    protected $secure = true;
+
+    public function __construct(\ParagonIE\EasyDB\EasyDB $db, $session_cache, int $cachetime = null, bool $secure = null)
     {
         $this->db = $db;
 
         $this->session_cache = $session_cache;
 
+        if (!empty($cachetime)) {
+            $this->cachetime = $cachetime;
+        }
+
+        if (!is_null($secure)) {
+            $this->secure = $secure;
+        }
+
     }
 
-    public function open($save_path = null, $id)
+    public function open($save_path = null, $id = null)
     {
-        //var_dump($save_path, $id);
-        //$this->db->insert('sessions', ['id' => $id, 'data' => NULL, 'timestamp' => time()]);
-
-        return true;
-    }
-
-    public function read($id)
-    {
-        //return $this->db->row("SELECT data FROM sessions WHERE id = ?", $id);
-        //use cache
-        var_dump($id);
-        var_dump(json_decode($this->session_cache->fetch($this->session_cache_identifier . $id)));
-
         return true;
     }
 
@@ -42,39 +41,44 @@ class session extends \SessionHandler
         return true;
     }
 
+    public function read($id)
+    {
+        //use cache
+        if ($this->session_cache->contains($this->session_cache_identifier . $id)) {
+            return $this->session_cache->fetch($this->session_cache_identifier . $id);
+        } else {
+            //try reading from db
+            if ($data = $this->db->row("SELECT data FROM sessions WHERE id = ?", $id)) {
+                var_dump("in db");
+                $this->session_cache->save($this->session_cache_identifier . $id, $data, $this->cachetime);
+                return $data['data'];
+            }
+        }
+        return false;
+    }
+
     public function write($id, $data)
     {
         //do a dumb write for now
-        var_dump($id);
-        if (!empty($data)) {
-            $data_json = json_encode($data);
-        } else {
-            $data_json = null;
-        }
         if ($this->db->row("SELECT id FROM sessions WHERE id = ?", $id)) {
-            var_dump("yes");
-            $this->db->update('sessions', ['data' => $data_json], ['id' => $id]);
+            $this->db->update('sessions', ['data' => $data], ['id' => $id]);
         } else {
-            var_dump("no");
+            $this->destroy($id);
             $this->db->insert('sessions', [
                 'id' => $id,
-                'data' => $data_json,
+                'data' => $data,
                 'timestamp' => time()
             ]);
         }
         //update the cache
 
-        $this->session_cache->save($this->session_cache_identifier . $id, $data_json);
-        //debug
-        return true;
+        return $this->session_cache->save($this->session_cache_identifier . $id, $data, $this->cachetime);
     }
 
     public function destroy($id)
     {
         $this->db->delete('sessions', ['id' => $id]);
-        $this->session_cache->delete($this->session_cache_identifier . $id);
-
-        return true; // debug
+        return $this->session_cache->delete($this->session_cache_identifier . $id);
     }
 
     public function gc($lifetime)
@@ -90,7 +94,7 @@ class session extends \SessionHandler
     public function startsession()
     {
         $cookieParams = session_get_cookie_params();
-        session_set_cookie_params($cookieParams["lifetime"], "/", $cookieParams["domain"], FALSE, FALSE);
+        session_set_cookie_params($cookieParams["lifetime"], "/", $cookieParams["domain"], $this->secure, TRUE);
         session_name("id");
         session_start();
     }
