07a4cf626c2270e544e7b9d140b2a1ad4ef18cd2
Skuza <skuza@emonit.de>
2017-05-31 17:54:05 +0200

    update mysql.sh

--- tests/mysql.sh
+++ tests/mysql.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-mysql -u root <<EOF
-CREATE DATABASE dev;
-USE dev;
-CREATE TABLE `sessions` (
-  `id` char(64) NOT NULL,
-  `data` mediumtext DEFAULT NULL,
-  `timestamp` int(10) unsigned NOT NULL,
-  `remember_me` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
-EOF
+mysql -u root -e "CREATE DATABASE dev;USE dev;CREATE TABLE `sessions` (`id` char(64) NOT NULL,`data` mediumtext DEFAULT NULL,`timestamp` int(10) unsigned NOT NULL,`remember_me` tinyint(1) NOT NULL DEFAULT 0) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"
+
 
