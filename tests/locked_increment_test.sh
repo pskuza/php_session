68bf52c4186adfe88d64b86b3e0b6158354c67e7
Skuza <skuza@emonit.de>
2017-06-01 22:08:25 +0200

    debug

--- tests/locked_increment_test.sh
+++ tests/locked_increment_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-seq 20 | parallel -n0 -j5 -q "curl -c cookie.jar -b cookie.jar http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true"
+seq 20 | parallel -n0 -j5 -q "curl -c cookie.jar -b cookie.jar http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=false"
 
-curl -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
+curl -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=false"
 
 exit 0
