24ba48eac75b057b51fb81173e155c9fba946ea0
Skuza <skuza@emonit.de>
2017-06-02 17:03:43 +0200

    some more tests

--- tests/locked_increment_test.sh
+++ tests/locked_increment_test.sh
@@ -5,6 +5,8 @@ rm -f cookie.jar
 
 #easy way I guess since parallel did not want to work for me
 curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true"
+#fix racy condition in the test
+sleep 1
 for i in {1..19};
 do
     curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
