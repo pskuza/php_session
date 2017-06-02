fd5f4ea603cb5ccfb1676da12f9978ff3392c074
Skuza <skuza@emonit.de>
2017-06-02 10:41:59 +0200

    will it work now on travis?

--- tests/locked_increment_test.sh
+++ tests/locked_increment_test.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-#easy way I guess sind parallel did not want to work for me
+#easy way I guess since parallel did not want to work for me
 for i in {1..20};
 do
-    curl -s -c /tmp/cookie.jar -b /tmp/cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
+    curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
 done
 
 wait
 
 #get result
-curl -s -c /tmp/cookie.jar -b /tmp/cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
+curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
 
 exit 0
