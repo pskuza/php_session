9efbe01d555704ac72b1e11be600feacbcfa2bca
Skuza <skuza@emonit.de>
2017-06-02 10:47:57 +0200

    why does the session not stick

--- tests/locked_increment_test.sh
+++ tests/locked_increment_test.sh
@@ -3,13 +3,13 @@
 #easy way I guess since parallel did not want to work for me
 for i in {1..20};
 do
-    curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
+    curl -v -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
 done
 
 wait
 
 #get result
-curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
+curl -v -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
 
 #debug dump database
 
