30da00462be62e5b2e3cb7399c313bfef4741e91
Skuza <skuza@emonit.de>
2017-06-02 10:53:54 +0200

    is this the cause?

--- tests/locked_increment_test.sh
+++ tests/locked_increment_test.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 #easy way I guess since parallel did not want to work for me
+curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true"
 for i in {1..20};
 do
-    curl -v -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
+    curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=4&locking=true" &
 done
 
 wait
 
 #get result
-curl -v -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
-
-#debug dump database
-
-mysql -u root dev -e "Select * FROM sessions;"
+curl -s -c cookie.jar -b cookie.jar "http://127.0.0.1:8080/SessionMysql.php?tests=5&locking=true"
 
 exit 0
