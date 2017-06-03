4dc72881d66e0dc08f843ea42c71506d3687179c
Skuza <skuza@emonit.de>
2017-06-03 15:37:07 +0200

    optimize table

--- tests/mysql.sql
+++ tests/mysql.sql
@@ -2,7 +2,9 @@ CREATE TABLE `sessions` (
   `id`          CHAR(64)            NOT NULL,
   `data`        MEDIUMTEXT                   DEFAULT NULL,
   `timestamp`   INT(10) UNSIGNED    NOT NULL,
-  `remember_me` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+  `remember_me` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  INDEX `garbage_collection_index` (`remember_me`, `timestamp`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
