601e89705b923161eeb98c2d6bfdc9da8667150d
Skuza <skuza@emonit.de>
2017-06-08 14:17:44 +0200

    change names

--- tests/SessionMysqlMemcachedTest.php
+++ tests/SessionMysqlMemcachedTest.php
@@ -15,7 +15,7 @@ class SessionMysqlTest extends TestCase
 
         $random_hex = bin2hex(random_bytes(8));
 
-        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionMysql.php?tests=0&locking=false&random='.$random_hex);
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionMysql.php?tests=0&locking=false&random=' . $random_hex);
 
         $headers = $r->getHeaders();
 
