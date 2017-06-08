fe076727e6cca6fb94ce2fa287a26627f2e46dab
Skuza <skuza@emonit.de>
2017-06-08 14:14:42 +0200

    add redis cache test

---
+++ tests/SessionMysqlRedis.php
@@ -0,0 +1,60 @@
+<?php
+
+use php_session\session;
+use PHPUnit\Framework\TestCase;
+
+class SessionRedisTest extends TestCase
+{
+    public function testSessions()
+    {
+        //run php web server in tests dir
+        shell_exec('cd tests && php -S 127.0.0.1:8080 >/dev/null 2>/dev/null &');
+        //give php some time for the webserver
+        sleep(5);
+        $client = new GuzzleHttp\Client(['cookies' => true]);
+
+        $random_hex = bin2hex(random_bytes(8));
+
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=0&locking=false&random=' . $random_hex);
+
+        $headers = $r->getHeaders();
+
+        //is the session getting created & set works
+        $this->assertArrayHaskey('Set-Cookie', $headers, 'Session start failed.');
+
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=1&locking=false');
+
+        $headers_new = $r->getHeaders();
+
+        //does regenerate_id work
+        $this->assertTrue($headers['Set-Cookie'] !== $headers_new['Set-Cookie'], 'Regenerate id failed.');
+
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=2&locking=false');
+
+        //does get work
+        $this->assertTrue($random_hex === $r->getBody()->getContents(), 'Session storage did not save or get.');
+
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=3&locking=false');
+
+        $headers = $r->getHeaders();
+
+        //does logout work
+        $this->assertArrayNotHasKey('Cookie', $headers, 'Logout did not work.');
+
+        //does locking work
+        $output = shell_exec('bash tests/locked_increment_test.sh');
+        $this->assertEquals('20', $output, 'Session locking feature did not lock correctly.');
+
+        //does remember_me work
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=6&locking=false');
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=7&locking=false');
+
+        $this->assertEquals('1', $r->getBody()->getContents(), 'Remember me was not set to 1 in DB.');
+
+        //does now setting remember me to 0 work
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=8&locking=false');
+        $r = $client->request('GET', 'http://127.0.0.1:8080/SessionRedis.php?tests=7&locking=false');
+
+        $this->assertEquals('0', $r->getBody()->getContents(), 'Remember me was not set to 0 in DB.');
+    }
+}
---
+++ tests/SessionRedis.php
@@ -0,0 +1,71 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require '../vendor/autoload.php';
+
+use php_session\session;
+
+$redis = new Redis();
+$redis->connect('redis_host', 6379);
+
+$cacheDriver = new \Doctrine\Common\Cache\RedisCache();
+$cacheDriver->setRedis($redis);
+
+$db = \ParagonIE\EasyDB\Factory::create(
+    'mysql:host=localhost;dbname=dev',
+    'root',
+    ''
+);
+
+switch ($_GET['locking']) {
+    case 'true':
+        $session = new php_session\session($db, $cacheDriver, 0, false, true);
+        break;
+    case 'false':
+        $session = new php_session\session($db, $cacheDriver, 0, false);
+        break;
+}
+
+session_set_save_handler($session, true);
+
+$session->start();
+$session->generate_csrf();
+
+switch ($_GET['tests']) {
+    case 0:
+        $session->set(['tests' => $_GET['random']]);
+        break;
+    case 1:
+        $session->regenerate_id();
+        break;
+    case 2:
+        echo $session->get('tests');
+        break;
+    case 3:
+        $session->logout();
+        break;
+    case 4:
+        //increment a session variable
+        $count = $session->get('increment');
+        if (empty($count)) {
+            $session->set(['increment' => 1], true);
+        } else {
+            $count++;
+            $session->set(['increment' => $count], true);
+        }
+        break;
+    case 5:
+        echo $session->get('increment');
+        break;
+    case 6:
+        $session->remember_me(true);
+        break;
+    case 7:
+        echo $db->cell('SELECT remember_me FROM sessions WHERE id = ?', session_id());
+        break;
+    case 8:
+        $session->remember_me(false);
+        break;
+}
