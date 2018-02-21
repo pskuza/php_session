fe14f15aa399c384e7147d6b63b0ef8128b8ecb6
P.Skuza <philip.skuza@gmail.com>
2018-02-21 12:44:30 +0100

    this it?

--- tests/mysql.sql
+++ tests/mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE `sessions` (
   `id`          CHAR(64)            NOT NULL,
   `session_data`        TEXT                         DEFAULT NULL,
   `created_at`   DATETIME DEFAULT CURRENT_TIMESTAMP,
-  `remember_me` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  `remember_me` TINYINT(1) UNSIGNED DEFAULT 0,
   PRIMARY KEY (`id`),
   INDEX `garbage_collection_index` (`remember_me`, `created_at`)
 )
